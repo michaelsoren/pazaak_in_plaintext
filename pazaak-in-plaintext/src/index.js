49677567500a892445100ab07f56cb34712518b8
michaellemay1@me.com <michaellemay@Michaels-Mac-mini.local>
2021-01-02 22:53:46 -0800

    Days work, got the logic mostly working and implemented end game feature. Will need to add reset button and set tracker, and fix the logic

--- pazaak-in-plaintext/src/index.js
+++ pazaak-in-plaintext/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import chunk from 'lodash/chunk'
 
-const numRows = 4;
-const numCols = 4;
+const numRows = 3;
+const numCols = 3;
 
 function Card(props) {
   return (
@@ -14,11 +14,12 @@ function Card(props) {
   );
 }
 
-class Card extends React.Component {
+class Cards extends React.Component {
   renderCard(i) {
+    let dealtCard = this.props.dealtCards[i];
     return (
-      <Square
-        value={this.props.cards[i] !== 0 ? this.props.cards[i] : null}
+      <Card
+        value={dealtCard !== 0 ? dealtCard : null}
       />
     );
   }
@@ -26,12 +27,14 @@ class Card extends React.Component {
   render() {
     return (
       <div>
-        <div className="board">
+        <div className="cards">
           {
             chunk(new Array(numRows * numCols).fill(0), numCols).map((item, rowIndex) => {
               return (
                 <div key={rowIndex} className="row">
-                  {item.map((col, colIndex) => this.renderCard(numRows * rowIndex + colIndex))}
+                  {
+                  item.map((col, colIndex) => this.renderCard(numRows * rowIndex + colIndex))
+                  }
                 </div>
               )
             })
@@ -48,21 +51,21 @@ class Table extends React.Component {
       <div className="game-table">
         <div className="playerColumn">
           <div>{"Player One"}</div>
-          <div className="board">
-            <Board
-              cards={this.props.playerOneCards}
+          <div className="cards">
+            <Cards
+              dealtCards={this.props.playerOneCards}
             />
           </div>
-          <div>{"Total: "}</div>
+          <div>{"Total: " + this.props.playerOneCards.reduce((a, b) => a + b, 0)}</div>
         </div>
         <div className="playerColumn">
           <div>{"Player Two"}</div>
-          <div className="board">
-            <Board
-              cards={this.props.playerTwoCards}
+          <div className="cards">
+            <Cards
+              dealtCards={this.props.playerTwoCards}
             />
           </div>
-          <div>{"Total: "}</div>
+          <div>{"Total: " + this.props.playerTwoCards.reduce((a, b) => a + b, 0)}</div>
         </div>
       </div>
     );
@@ -72,92 +75,214 @@ class Table extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    let playerOneInitialCards = Array(numRows * numCols).fill(0);
+    let playerTwoInitialCards = Array(numRows * numCols).fill(0);
+    let newCard = getNewCard();
+    playerOneInitialCards[0] = newCard;
     this.state = {
-      playerOneCards: Array(numRows * numCols).fill(0),
-      playerTwoCards: Array(numRows * numCols).fill(0),
-      isPlayerOneStanding: false,
-      isPlayerTwoStanding: false,
-      oneIsNext: true
+      playerCards: [playerOneInitialCards, playerTwoInitialCards],
+      isPlayerStanding: [false, false],
+      playerCardIndexes: [1, 0],
+      currentPlayer: 0,
+      winner: null,
     };
   }
 
-  handleForfeit(player) {
-    if (calculateWinner(this.props.playerOneCards.reduce((a, b) => a + b, 0), this.props.isPlayerOneStanding, this.props.playerTwoCards.reduce((a, b) => a + b, 0), this.props.isPlayerTwoStanding)) {
-      return;
-    }
-
-    if (player === 1) {
-      this.setState();
-    } else {
-      this.setState();
-    }
-  }
-
-  handleEndTurn(player) {
-    if (calculateWinner(this.props.playerOneCards.reduce((a, b) => a + b, 0), this.props.isPlayerOneStanding, this.props.playerTwoCards.reduce((a, b) => a + b, 0), this.props.isPlayerTwoStanding)) {
-      return;
-    }
-
-    if (player === 1) {
+  handleForfeit(playerIndex) {
+    if (playerIndex === 1) {
       this.setState({
-        playerOneCards: this.props.playerOneCards,
-        playerTwoCards: this.props.playerTwoCards,
-        oneIsNext: true,
+        winner: "Two",
       });
     } else {
-      this.setState();
+      this.setState({
+        winner: "One",
+      });
     }
   }
 
-  handleStand(player) {
-    if (calculateWinner(this.props.playerOneCards.reduce((a, b) => a + b, 0), this.props.isPlayerOneStanding, this.props.playerTwoScore.reduce((a, b) => a + b, 0), this.props.isPlayerTwoStanding)) {
-      return;
-    }
+  handleEndTurn(playerIndex) {
+    let newCard = getNewCard();
+    let otherPlayerIndex = getOtherPlayerIndex(playerIndex);
+    console.log("This PlayerIndex: " + playerIndex + ", Player Card Indexes: " + this.state.playerCardIndexes + ", isPlayerOneStanding: " + this.state.isPlayerStanding + ", playerCards: " + this.state.playerCards + ", currentPlayer: " + this.state.currentPlayer);
+    //Player ends turn, set up for other player's turn, splitting on whether they are standing or not
+    if (!this.state.isPlayerStanding[otherPlayerIndex]) {
+      //other player is not standing, deal them the card, set up, and make it their turn
+      let otherPlayerNewCards = this.state.playerCards[otherPlayerIndex].slice();
+      otherPlayerNewCards[this.state.playerCardIndexes[otherPlayerIndex]] = newCard;
+      let otherPlayerTotal = otherPlayerNewCards.reduce((a, b) => a + b, 0);
+      //check if other player has reached 20
+      if (otherPlayerTotal === 20) {
+        //automatically stand if other player has 20. Draw and give first player their card, set to be their turn
+        let secondCard = getNewCard();
+        let originalPlayerNewCards = this.state.playerCards[playerIndex].slice();
+        originalPlayerNewCards[this.state.playerCardIndexes[playerIndex]] = secondCard;
+        let originalPlayerTotal = otherPlayerNewCards.reduce((a, b) => a + b, 0);
+        let isOriginalPlayerStanding = false;
+        if (originalPlayerTotal === 20) {
+          isOriginalPlayerStanding = true;
+        }
 
-    if (player === 1) {
-      this.setState();
+        let newPlayerCards = this.state.playerCards.slice();
+        newPlayerCards[playerIndex] = originalPlayerNewCards;
+        newPlayerCards[otherPlayerIndex] = otherPlayerNewCards;
+
+        let newPlayerCardIndexes = this.state.playerCardIndexes.slice();
+        newPlayerCardIndexes[playerIndex] += 1;
+        newPlayerCardIndexes[otherPlayerIndex] += 1;
+
+        let newIsPlayerStanding = this.state.isPlayerStanding.slice();
+        newIsPlayerStanding[playerIndex] = isOriginalPlayerStanding;
+        newIsPlayerStanding[otherPlayerIndex] = true;
+
+        let winner = calculateWinner(newPlayerCards, newIsPlayerStanding);
+
+        this.setState({
+          playerCards: newPlayerCards,
+          playerCardIndexes: newPlayerCardIndexes,
+          isPlayerStanding: newIsPlayerStanding,
+          currentPlayer: playerIndex, //todo
+          winner: winner,
+        });
+      } else {
+        //other player total is not twenty, so set it to be their turn with their new card
+
+        let newPlayerCards = this.state.playerCards.slice();
+        newPlayerCards[otherPlayerIndex] = otherPlayerNewCards;
+
+        let newPlayerCardIndexes = this.state.playerCardIndexes.slice();
+        newPlayerCardIndexes[otherPlayerIndex] += 1;
+
+        let winner = calculateWinner(newPlayerCards, this.state.isPlayerStanding);
+        this.setState({
+          playerCards: newPlayerCards,
+          playerCardIndexes: newPlayerCardIndexes,
+          currentPlayer: otherPlayerIndex, //todo
+          winner: winner,
+        });
+      }
     } else {
-      this.setState();
+      //other player is standing, don't deal them the new card. Instead deal to original player again. Check if they got 20.
+      let originalPlayerNewCards = this.state.playerCards[playerIndex].slice();
+      originalPlayerNewCards[this.state.playerCardIndexes[playerIndex]] = newCard;
+      let originalPlayerTotal = originalPlayerNewCards.reduce((a, b) => a + b, 0);
+      let isOriginalPlayerStanding = false;
+      if (originalPlayerTotal === 20) {
+        isOriginalPlayerStanding = true;
+      }
+
+      let newPlayerCards = this.state.playerCards.slice();
+      newPlayerCards[otherPlayerIndex] = originalPlayerNewCards;
+
+      let newPlayerCardIndexes = this.state.playerCardIndexes.slice();
+      newPlayerCardIndexes[playerIndex] += 1;
+
+      let newIsPlayerStanding = this.state.isPlayerStanding.slice();
+      newIsPlayerStanding[playerIndex] = isOriginalPlayerStanding;
+
+      let winner = calculateWinner(newPlayerCards, newIsPlayerStanding);
+
+      this.setState({
+        playerCards: newPlayerCards,
+        isPlayerStanding: newIsPlayerStanding,
+        playerCardIndexes: newPlayerCardIndexes,
+        currentPlayer: playerIndex,
+        winner: winner,
+      });
     }
   }
 
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      oneIsNext: (step % 2) === 0
-    });
+  handleStand(playerIndex) {
+    console.log("This PlayerIndex: " + playerIndex + ", Player Card Indexes: " + this.state.playerCardIndexes + ", isPlayerOneStanding: " + this.state.isPlayerStanding + ", playerCards: " + this.state.playerCards + ", currentPlayer: " + this.state.currentPlayer);    
+    let newCard = getNewCard();
+    let otherPlayerIndex = getOtherPlayerIndex(playerIndex);
+
+    //If other player is not standing, draw their card
+    if (!this.state.isPlayerStanding[otherPlayerIndex]) {
+      let otherPlayerNewCards = this.state.playerCards[otherPlayerIndex].slice();
+      otherPlayerNewCards[this.state.playerCardIndexes[otherPlayerIndex]] = newCard;
+      let otherPlayerTotal = otherPlayerNewCards.reduce((a, b) => a + b, 0);
+      let isOtherPlayerStanding = false;
+      //If 20, then both players are already standing. Don't need to return to draw a new card for original player
+      if (otherPlayerTotal === 20) {
+        isOtherPlayerStanding = true;
+      }
+
+      let newPlayerCards = this.state.playerCards.slice();
+      newPlayerCards[otherPlayerIndex] = otherPlayerNewCards;
+
+      let newPlayerCardIndexes = this.state.playerCardIndexes.slice();
+      newPlayerCardIndexes[otherPlayerIndex] += 1;
+
+      let newIsPlayerStanding = this.state.isPlayerStanding.slice();
+      newIsPlayerStanding[playerIndex] = true;
+      newIsPlayerStanding[otherPlayerIndex] = isOtherPlayerStanding;
+
+
+      let winner = calculateWinner(newPlayerCards, newIsPlayerStanding);
+      
+      this.setState({
+        playerCards: newPlayerCards,
+        newPlayerCardIndexes: newPlayerCardIndexes,
+        newIsPlayerStanding: newIsPlayerStanding,
+        currentPlayer: false,
+        winner: winner,
+      });
+    } else {
+      //both players are now standing, calculate winners and update the state
+      let newIsPlayerStanding = this.state.isPlayerStanding.slice();
+      newIsPlayerStanding[playerIndex] = true;
+
+      let winner = calculateWinner(this.state.playerCards, newIsPlayerStanding);
+
+      this.setState({
+        newIsPlayerStanding: newIsPlayerStanding,
+        currentPlayer: otherPlayerIndex,
+        winner: winner,
+      });
+    }
   }
 
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
     let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Current player: " + (this.state.oneIsNext ? "One" : "Two");
-    }
-
-    return (
-      <div className="game">
-        <div>
-          <Table
-            playerOneCards={this.props.playerOneCards}
-            playerTwoCards={this.props.playerTwoCards}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <div className="game-buttons">
-              <button className="game-button" onClick={() => this.handleEndTurn(0)}>{"End Turn"}</button>
-              <button className="game-button" onClick={() => this.handleStand(0)}>{"Stand"}</button>
-              <button className="game-button" onClick={() => this.handleForfeit(0)}>{"Forfeit"}</button>
+    if (this.state.winner) {
+      status = "Winner: " + this.state.winner;
+      return (
+        <div className="game">
+          <div>
+            <Table
+              playerOneCards={this.state.playerCards[0]}
+              playerTwoCards={this.state.playerCards[1]}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <div className="game-buttons">
+                <button className="game-button">{"Play Again?"}</button>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      status = "Current player: " + (this.state.currentPlayer === 0 ? "One" : "Two");
+      return (
+        <div className="game">
+          <div>
+            <Table
+              playerOneCards={this.state.playerCards[0]}
+              playerTwoCards={this.state.playerCards[1]}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <div className="game-buttons">
+                <button className="game-button" onClick={() => this.handleEndTurn(this.state.currentPlayer)}>{"End Turn"}</button>
+                <button className="game-button" onClick={() => this.handleStand(this.state.currentPlayer)}>{"Stand"}</button>
+                <button className="game-button" onClick={() => this.handleForfeit(this.state.currentPlayer)}>{"Forfeit"}</button>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
@@ -165,15 +290,32 @@ class Game extends React.Component {
 
 ReactDOM.render(<Game />, document.getElementById("root"));
 
-function calculateWinner(pOneScore, pOneStanding, pTwoScore, pTwoStanding) {
-  if (pOneScore > 20) {
+function getNewCard() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+function getOtherPlayerIndex(index) {
+  return (index + 1) % 2;
+}
+
+function calculateWinner(playerCards, isPlayerStanding) {
+  let playerOneScore = playerCards[0].reduce((a, b) => a + b, 0);
+  let playerTwoScore = playerCards[1].reduce((a, b) => a + b, 0);
+  let isPlayerOneStanding = isPlayerStanding[0];
+  let isPlayerTwoStanding = isPlayerStanding[1];
+
+  if (playerOneScore > 20) {
     return "Two";
-  } else if (pTwoScore > 20) {
+  } else if (playerTwoScore > 20) {
     return "One";
-  } else if (pOneStanding && pOneScore < pTwoScore) {
+  } else if (playerOneScore === 20 && playerTwoScore === 20) {
+    return "Tie";
+  } else if (isPlayerOneStanding && playerOneScore < playerTwoScore) {
     return "Two";
-  } else if (pTwoStanding && pOneScore > pTwoScore) {
-    return "Two";
+  } else if (isPlayerTwoStanding && playerOneScore > playerTwoScore) {
+    return "One";
+  } else if (isPlayerOneStanding && isPlayerTwoStanding && playerOneScore === playerTwoScore) {
+    return "Tie";
   } else {
     return null;
   }
